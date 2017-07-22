944fe7d8a3e0ac4fd412d7c1fce8ac07b06f04f1
Lucia Noelia Bressan <lucyluz333@gmail.com>
2017-07-22 18:20:33 -0300

    Commit original

---
+++ src/main/java/ar/edu/unrc/coeus/xor/Action.java
@@ -0,0 +1,18 @@
+package ar.edu.unrc.coeus.xor;
+
+import ar.edu.unrc.coeus.tdlearning.interfaces.IAction;
+
+public
+enum Action
+        implements IAction {
+
+    /**
+     * Cero
+     */
+    CERO,
+    /**
+     * Uno
+     */
+    UNO
+}
+
---
+++ src/main/java/ar/edu/unrc/coeus/xor/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unrc.coeus.xor;
+
+public
+class Main {
+
+    public static
+    void main( String[] args ) {
+        System.out.println("Hello World!");
+    }
+}
