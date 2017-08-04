e4aa5d52a624444d6dfe24e87833b0c692051a1e
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2017-08-04 03:05:36 -0300

    Limpieza de código

--- src/test/java/ar/edu/unrc/coeus/pig/GameStateTest.java
+++ src/test/java/ar/edu/unrc/coeus/pig/GameStateTest.java
@@ -1,40 +1,39 @@
 package ar.edu.unrc.coeus.pig;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static ar.edu.unrc.coeus.pig.Game.*;
 
 public
 class GameStateTest {
-    private
+    private static
     void inputTestFor(
-            int player1Score,
-            int player2Score,
-            int player1TotalReward,
-            int player2TotalReward,
-            int player
+            final int player1Score,
+            final int player2Score,
+            final int player1TotalReward,
+            final int player2TotalReward,
+            final int player
     ) {
-        GameState state;
-        state = new GameState(0, player == 0, player1Score, player2Score, player1TotalReward, player2TotalReward);
-        int count = 0;
+        final GameState state = new GameState(0, player == 0, player1Score, player2Score, player1TotalReward, player2TotalReward);
+        int             count = 0;
         for ( int i = 0; i < INPUT_NEURONS; i++ ) {
-            if ( state.translateToPerceptronInput(i) == 1 ) {
+            if ( state.translateToPerceptronInput(i) == 1.0 ) {
                 count++;
             }
         }
         if ( count != 5 ) {
-            TestCase.fail(state.toString());
+            Assert.fail(state.toString());
         }
     }
 
     @Test
-    public
+    public final
     void translateToPerceptronInput()
             throws Exception {
 
-        int[] scoreToTest     = { 0, 1, 2, 3, MAX_SCORE - 3, MAX_SCORE - 2, MAX_SCORE - 1, MAX_SCORE };
-        int[] maxRewordToTest = { 0, 1, 2, 3, MAX_REWARD - 3, MAX_REWARD - 2, MAX_REWARD - 1, MAX_REWARD };
+        final int[] scoreToTest     = { 0, 1, 2, 3, MAX_SCORE - 3, MAX_SCORE - 2, MAX_SCORE - 1, MAX_SCORE };
+        final int[] maxRewordToTest = { 0, 1, 2, 3, MAX_REWARD - 3, MAX_REWARD - 2, MAX_REWARD - 1, MAX_REWARD };
 
         for ( int player1Score = 0; player1Score < scoreToTest.length; player1Score++ ) {
             for ( int player2Score = 0; player2Score < scoreToTest.length; player2Score++ ) {
